924cf5e0cf186835d3932a989793a409cdd78016
Yara Hamza <yaraassadhamza@gmail.com>
2026-04-04 14:48:33 +0300

    feat: configure static export for Next.js and add GitHub Actions workflow for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/GraduationProject-AFAQ",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
